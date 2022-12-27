9bab903b651df57eaceaffbf7ce0d0b67e77910b
syhrlanwr <syhrlanwr@gmail.com>
2022-12-27 21:09:12 +0700

    implement api services

--- app/src/main/java/lat/pam/pizzarestaurant/MainActivity3.java
+++ app/src/main/java/lat/pam/pizzarestaurant/MainActivity3.java
@@ -5,12 +5,19 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity3 extends AppCompatActivity {
+import java.util.Collections;
+import java.util.List;
 
-    private Toolbar toolbar;
+import lat.pam.pizzarestaurant.models.Menu;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,65 +27,104 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        TextView textView = (TextView) findViewById(R.id.user_name);
-        textView.setText(userName);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        TextView peperoniTitle = (TextView) findViewById(R.id.peperoni_title);
+        TextView peperoniDetails = (TextView) findViewById(R.id.peperoni_description);
+
+        TextView spaghettiTitle = (TextView) findViewById(R.id.spaghetti_title);
+        TextView spaghettiDetails = (TextView) findViewById(R.id.spaghetti_description);
+
+        TextView burgerTitle = (TextView) findViewById(R.id.burger_title);
+        TextView burgerDetails = (TextView) findViewById(R.id.burger_description);
+
+        TextView steakTitle = (TextView) findViewById(R.id.steak_title);
+        TextView steakDetails = (TextView) findViewById(R.id.steak_description);
 
         View pepperoni = findViewById(R.id.pepperoni);
-        pepperoni.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
-                intent.putExtra("userName", userName);
-                intent.putExtra("city", city);
-                intent.putExtra("pizza", "Pepperoni Pizza");
-                intent.putExtra("price", "Rp. 100.000");
-                intent.putExtra("description", "Pepperoni pizza adalah pizza yang memiliki  topping daging pepperoni sapi asli. Di Italia, pepperoni disebut salame piccante (salami panas). Biasa menjadi bahan baku pizza di Amerika Serikat, yang sering mewakili 30% pelengkap. Pizza ini cocok untuk santap siang ataupun malam anda");
-                startActivity(intent);
-            }
-        });
-
         View spaghetti = findViewById(R.id.spaghetti);
-        spaghetti.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
-                intent.putExtra("userName", userName);
-                intent.putExtra("city", city);
-                intent.putExtra("pizza", "Spaghetti");
-                intent.putExtra("price", "Rp. 80.000");
-                intent.putExtra("description", "Spaghetti merupakan salah satu dari varian pasta yang memiliki bentuk silinder tipis dan padat yang terbuat dari olahan gandum. Spaghetti telah menjadi makanan pokok tradisional penduduk Italia.");
-                startActivity(intent);
-            }
-        });
-
         View burger = findViewById(R.id.burger);
-        burger.setOnClickListener(new View.OnClickListener() {
+        View steak = findViewById(R.id.steak);
+
+
+        Call<List<Menu>> call = RetrofitClient.getInstance().getMyApi().getMenu();
+        call.enqueue(new Callback<List<Menu>>() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
-                intent.putExtra("userName", userName);
-                intent.putExtra("city", city);
-                intent.putExtra("pizza", "Burger");
-                intent.putExtra("price", "Rp. 50.000");
-                intent.putExtra("description", "Burger adalah makanan yang terbuat dari daging sapi yang dibentuk menjadi bentuk bulat dan digoreng. Burger biasanya disajikan dengan bahan tambahan seperti keju, tomat, selada, dan saus.");
-                startActivity(intent);
+            public void onResponse(Call<List<Menu>> call, Response<List<Menu>> response) {
+                List<Menu> menus = response.body();
+                peperoniTitle.setText(menus.get(0).getFoodName());
+                peperoniDetails.setText(menus.get(0).getDetails());
+
+                pepperoni.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                        intent.putExtra("userName", userName);
+                        intent.putExtra("city", city);
+                        intent.putExtra("pizza", menus.get(0).getFoodName());
+                        intent.putExtra("price", "Rp. "+menus.get(0).getPrice());
+                        intent.putExtra("description", menus.get(0).getDetails());
+                        startActivity(intent);
+                    }
+                });
+
+                spaghettiTitle.setText(menus.get(1).getFoodName());
+                spaghettiDetails.setText(menus.get(1).getDetails());
+
+                spaghetti.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                        intent.putExtra("userName", userName);
+                        intent.putExtra("city", city);
+                        intent.putExtra("pizza", menus.get(1).getFoodName());
+                        intent.putExtra("price", "Rp. "+menus.get(1).getPrice());
+                        intent.putExtra("description", menus.get(1).getDetails());
+                        startActivity(intent);
+                    }
+                });
+
+                burgerTitle.setText(menus.get(2).getFoodName());
+                burgerDetails.setText(menus.get(2).getDetails());
+
+                burger.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                        intent.putExtra("userName", userName);
+                        intent.putExtra("city", city);
+                        intent.putExtra("pizza", menus.get(2).getFoodName());
+                        intent.putExtra("price", "Rp. "+menus.get(2).getPrice());
+                        intent.putExtra("description", menus.get(2).getDetails());
+                        startActivity(intent);
+                    }
+                });
+
+                steakTitle.setText(menus.get(3).getFoodName());
+                steakDetails.setText(menus.get(3).getDetails());
+
+                steak.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                        intent.putExtra("userName", userName);
+                        intent.putExtra("city", city);
+                        intent.putExtra("pizza", menus.get(3).getFoodName());
+                        intent.putExtra("price", "Rp. "+menus.get(3).getPrice());
+                        intent.putExtra("description", menus.get(3).getDetails());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<List<Menu>> call, Throwable t) {
+                Log.e("MainActivity", t.getMessage());
             }
         });
 
-        View steak = findViewById(R.id.steak);
-        steak.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
-                intent.putExtra("userName", userName);
-                intent.putExtra("city", city);
-                intent.putExtra("pizza", "Steak");
-                intent.putExtra("price", "Rp. 150.000");
-                intent.putExtra("description", "Steak adalah daging sapi yang dipotong tipis dan digoreng. Steak biasanya disajikan dengan bahan tambahan seperti keju, tomat, selada, dan saus.");
-                startActivity(intent);
-            }
-        });
+
+        TextView textView = (TextView) findViewById(R.id.user_name);
+        textView.setText(userName);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
