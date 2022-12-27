ee6e972d04adc4fc8830a520230f48c40659fbed
syhrlanwr <syhrlanwr@gmail.com>
2022-12-27 21:08:49 +0700

    Add api interfaces

---
+++ app/src/main/java/lat/pam/pizzarestaurant/Api.java
@@ -0,0 +1,14 @@
+package lat.pam.pizzarestaurant;
+
+import java.util.List;
+
+import lat.pam.pizzarestaurant.models.Menu;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+
+    String BASE_URL = "https://retoolapi.dev/StWODX/";
+    @GET("uasresto")
+    Call<List<Menu>> getMenu();
+}
